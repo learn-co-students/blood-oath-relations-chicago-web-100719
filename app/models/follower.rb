8f37e4cc12c591575437744f2ee2bb40ecea659b
benjewman <bennyadave@gmail.com>
2019-10-14 00:27:24 -0500

    with advanced methods

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -15,15 +15,34 @@ class Follower
     end
 
     def cults
-        blood_oaths = Blood_Oath.all.select {|b| b.follower == self}
+        blood_oaths = BloodOath.all.select {|b| b.follower == self}
         blood_oaths.collect {|b| b.cult}
     end
 
     def join_cult(cult)
-        Blood_Oath.new(cult, self, Date.today)
+        BloodOath.new(cult, self, Date.today)
     end
 
     def self.of_a_certain_age(age)
         self.all.select {|f| f.age >= age}
+    end
+
+    def my_cults_slogans
+        self.cults.each { |c| puts c.slogan }
+    end
+
+    def self.most_active
+        # calling first element of sorted array using my method
+        self.most_to_least_cults[0]
+        # self.all.max { |f, g| f.cults.count <=> g.cults.count }
+    end
+
+    def self.most_to_least_cults
+        # iterate followers to sort them by most cults
+        self.all.sort { |f, g| f.cults.count <=> g.cults.count }.reverse
+    end
+
+    def self.top_ten
+        self.most_to_least_cults[0..9]
     end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -11,10 +11,31 @@ end
 # making cults to test
 j_for_j = Cult.new("Jews for Jesus", "Jerusalem", 1969, "Jews need a king")
 koolaid = Cult.new("Kool-Aid Krew", "Atlanta", 2010, "we just like the way it tastes")
-
+manson_family = Cult.new("The Manson Family", "Atlanta", 1965, "take acid and kill")
 # making followers to test
 brad = Follower.new("Brad", 27, "good vibes")
 jane = Follower.new("Jane", 40, "I'm no Karen")
+tex = Follower.new("Tex", 18, "I'd love some LSD")
+tim = Follower.new("Time", 33, "o boy a cult")
+alex = Follower.new("Alex", 31, "be sexy or be sexy at it")
+nathan = Follower.new("Nathan", 26, "Atlanta sux")
+mom = Follower.new("Laura", 60, "come home")
+anna = Follower.new("Anna", 24, "are you mad at me?")
+hope = Follower.new("Hope", 25, "I'm low maintenance")
+ryan = Follower.new("Ryan", 27, "call me coach")
+ethan = Follower.new("Ethan", 26, "I make so much money")
+david = Follower.new("David", 22, "don't scoop me")
+j_for_j.recruit_follower(brad)
+jane.join_cult(j_for_j)
+tex.join_cult(manson_family)
+jane.join_cult(manson_family)
+tim.join_cult(manson_family)
+alex.join_cult(manson_family)
+nathan.join_cult(j_for_j)
+mom.join_cult(j_for_j)
+anna.join_cult(koolaid)
+hope.join_cult(j_for_j)
+ryan.join_cult(j_for_j)
 binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
